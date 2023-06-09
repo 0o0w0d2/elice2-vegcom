1f25a06f394678bb24701e720e0e1dad6a5e6256
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-06-09 15:08:00 +0900

    fix.localStorage -> sessionStorage

--- api.js
+++ api.js
@@ -7,9 +7,9 @@ axios.defaults.baseURL = 'http://localhost:5001/';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.put['Content-Type'] = 'application/json';
 
-// 토큰 불러오기(로그인 시 localStorage에 저장된 Token값을 불러오기)
+// 토큰 불러오기(로그인 시 sessionStorage에 저장된 Token값을 불러오기)
 const getToken = () => {
-    return localStorage.getItem('userToken');
+    return sessionStorage.getItem('userToken');
 };
 
 // GET (경로는 'baseURL/path'가 된다.)
