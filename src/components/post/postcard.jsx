d5448d1c3fa4f7b2ea6aa20626211b95a7c5900b
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-06-10 17:12:04 +0900

    feat.좋아요 기능 완료 but 리로딩이슈있음..

--- src/components/post/postcard.jsx
+++ src/components/post/postcard.jsx
@@ -5,26 +5,22 @@ import { ChatBubbleOvalLeftEllipsisIcon } from '@heroicons/react/24/outline';
 import { StarIcon as SolidStarIcon } from '@heroicons/react/24/solid';
 import { StarIcon } from '@heroicons/react/24/outline';
 import * as Api from '../../../api';
-import { comment } from 'postcss';
+// import { comment } from 'postcss';
 // import { formatPostcssSourceMap } from 'vite';
 
 function PostCard({ post }) {
     const userState = useContext(UserStateContext);
     const [comments, setComments] = useState([]);
+    const [likeCount, setLikeCount] = useState(0);
+    const [liked, setLiked] = useState(false);
     const navigate = useNavigate();
-<<<<<<< HEAD
+    const userId = userState.user.userId;
     // console.log(post);
-=======
->>>>>>> b374d586b9c89e18874a6c5c85c6b839cea7e7a9
 
     const handleClick = post => {
         navigate(`/post/${post.postId}`);
     };
 
-<<<<<<< HEAD
-    res = Api.get(`/like/${post.postId}`);
-    console.log(res);
-=======
     const getImageSrc = imageUrl => {
         if (imageUrl.startsWith('http')) {
             return imageUrl;
@@ -33,9 +29,21 @@ function PostCard({ post }) {
             return imageUrl;
         }
     };
->>>>>>> dbaf60ef2029b61a165e43125bee853cc56fe7a0
+    //likecount, likeuser
+    const fetchLikes = async post => {
+        try {
+            const res = await Api.get(`like/${post.postId}`);
+            console.log('like: ', res.data);
+            const likesData = res.data;
+            setLikeCount(likesData.likecount);
+            setLiked(likesData.likeuser);
+        } catch (err) {
+            alert('err.rseponse.data.message');
+            console.log('좋아요 불러오기를 실패했습니다.');
+        }
+    };
 
-    const fetchComments = async () => {
+    const fetchComments = async post => {
         try {
             const res = await Api.get(`/comment/${post.postId}`);
             const commentData = res.data.commentList;
@@ -49,63 +57,60 @@ function PostCard({ post }) {
 
     const handleLike = (postId, userId) => {
         try {
-            Api.post(`/like/${postId}`, {
-                // 좋아요 누르는 버튼 구현하기
-                postId,
-                userId,
-            });
+            if (liked === false) {
+                Api.post(`/like/${postId}`, {
+                    // 좋아요 누르는 버튼 구현하기
+                    postId,
+                    userId,
+                });
+                setLiked(true);
+            } else {
+                Api.del(`/like/${postId}`);
+                setLiked(false);
+            }
+            console.log('like 누르기 이후', liked);
         } catch (err) {
-            alert('err.response.data.message');
+            alert(err.message);
             console.log('좋아요 누르기 실패!');
         }
     };
 
-    // const fetchLikes = async () => {
-    //     try {
-    //         const res = await Api.get(`like/${post.postId}`);
-    //         const likesData = res.data;
-    //         console.log(res);
-    //     } catch (err) {
-    //         alert('err.rseponse.data.message');
-    //         console.log('좋아요 불러오기를 실패했습니다.');
-    //     }
-    // };
-
     useEffect(() => {
         // fetchLikes(post);
         fetchComments(post);
+        fetchLikes(post);
     }, [post.postId]);
 
     return (
         <div className="postCard rounded-lg mx-auto grid max-w-2xl grid-cols-1 border border-gray-300 pt-5 pl-5 pb-5 pr-5 mb-5">
             <article key={post.postId} className="flex max-w-xl flex-col items-start justify-between">
                 <div className="profileSection relative flex items-center gap-x-4">
-                    <img src={getImageSrc(post.userImage)} alt="User Image" className="h-10 w-10 rounded-full bg-gray-50" />
+                    <img src={post.userImage} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
                     <div style={{ display: 'flex', verticalAlign: 'middle' }}>{post.userId}</div>
                 </div>
                 <div className="postSection w-full">
-                    <img src={getImageSrc(post.imageUrl)} alt="Post Image" className="postImage w-full h-auto mt-5" />
+                    <img src={post.imageUrl} alt="Post Image" className="postImage w-full h-auto mt-5" />
                     <div className="flex mt-3">
                         {/* 눌렀을 때 좋아요 상태 변경하는 코드 추가하기 */}
-                        <StarIcon className="h-7 w-7" onClick={() => handleLike(post)} />
-                        {/* {post.like == true ? (
-                            <SolidStarIcon className="h-7 w-7" fill="#008762" />
+                        {/* <StarIcon className="h-7 w-7" onClick={() => handleLike(post)} /> */}
+                        {liked == true ? (
+                            <SolidStarIcon onClick={() => handleLike(post.postId, userId)} className="h-7 w-7" fill="#008762" />
                         ) : (
-                            <StarIcon className="h-7 w-7" />
-                        )} */}
+                            <StarIcon onClick={() => handleLike(post.postId, userId)} className="h-7 w-7" />
+                        )}
                         <ChatBubbleOvalLeftEllipsisIcon className="h-7 w-7" onClick={() => handleClick(post)} />
                     </div>
-                    {/* <div className="text-left mt-3">{post.postLikeCount.toLocaleString()} 명이 좋아합니다.</div> */}
+                    <div className="text-left mt-3">{likeCount.toLocaleString()} 명이 좋아합니다.</div>
                     <div className="flex mt-2 text-md text-left">
                         <span style={{ fontWeight: 'bold', marginRight: '0.4rem' }}>{post.userId}</span> {post.content}
                     </div>
                 </div>
                 <div>
-                    {comments.slice(0, 3)?.map(item => (
+                    {/* {comments.slice(0, 3)?.map(item => (
                         <div key={item.id}>
                             {item.nickname}: {item.content}
                         </div>
-                    ))}
+                    ))} */}
                 </div>
             </article>
         </div>
