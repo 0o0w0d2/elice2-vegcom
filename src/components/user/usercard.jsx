dbaf60ef2029b61a165e43125bee853cc56fe7a0
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-06-10 16:13:39 +0900

    Fix : UserCard에서 이미지를 불러오지 못하는 오류 수정

--- src/components/user/usercard.jsx
+++ src/components/user/usercard.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 const UserCard = ({ owner, point }) => {
+    const getImageSrc = () => {
+        if (owner.userImage.startsWith('http')) {
+            return owner.userImage;
+        } else {
+            owner.userImage = `https://7team-bucket.s3.ap-northeast-2.amazonaws.com/${user.userImage}`;
+            return owner.userImage;
+        }
+    };
     return (
         <div
             className="flex items-center justify-center p-4 m-2 bg-white shadow-2xl rounded-xl"
             style={{ width: '60vh', height: '60vh' }}>
             <div className="flex flex-col justify-center items-center text-center">
-                <img
-                    className="w-20 h-20 object-cover rounded-full mb-2"
-                    src={`https://7team-bucket.s3.ap-northeast-2.amazonaws.com/${owner.image}`}
-                    alt={owner.id}
-                />
+                <img className="w-20 h-20 object-cover rounded-full mb-2" src={getImageSrc()} alt={owner.id} />
                 <h2 className="text-lg font-bold">{owner.nickname}</h2>
                 <p className="text-sm text-gray-500">{point}</p>
             </div>
