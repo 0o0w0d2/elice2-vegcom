b97f2239b37e28e336c543a7406b71ca03fe21c7
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-06-12 14:15:08 +0900

    Feat : header의 메뉴 글자 클릭시 동작하던 기능을 메뉴 전체 div를 클릭하면 동작하도록 수정

--- src/sections/header.jsx
+++ src/sections/header.jsx
@@ -91,6 +91,7 @@ function Header() {
                                     {menus.map(item => (
                                         <div
                                             key={item.name}
+                                            onClick={() => navigate(item.href)}
                                             className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
                                             <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
                                                 <item.icon
@@ -99,11 +100,7 @@ function Header() {
                                                 />
                                             </div>
                                             <div>
-                                                <div
-                                                    className="font-semibold text-gray-900 text-left"
-                                                    onClick={() => navigate(item.href)}>
-                                                    {item.name}
-                                                </div>
+                                                <div className="font-semibold text-gray-900 text-left">{item.name}</div>
                                                 <p className="mt-1 text-gray-600">{item.description}</p>
                                             </div>
                                         </div>
