c7acb7d669c4b6315288c9bba50cc16cf18c9d24
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-06-13 17:36:40 +0900

    feat.포스트 무한스크롤 구현~~

--- src/pages/story/story.jsx
+++ src/pages/story/story.jsx
@@ -9,6 +9,7 @@ function Story() {
     const [postList, setPostList] = useState([]);
     const [nextCursor, setNextCursor] = useState(0);
     const [isLoading, setIsLoading] = useState(false);
+    const [isReached, setIsReached] = useState(false);
 
     const fetchPost = useCallback(
         async cursor => {
@@ -19,6 +20,13 @@ function Story() {
                 console.log('res:', res);
 
                 const postData = res.data;
+
+                if (postData.postList?.length < 5) {
+                    setNextCursor(-1);
+                } else {
+                    setNextCursor(postData.postList[postData.postList.length - 1].postId);
+                }
+
                 let newPostList;
 
                 if (cursor === 0) {
@@ -32,50 +40,48 @@ function Story() {
                 setPostList(newPostList);
 
                 console.log('newpostList', newPostList);
-                if (postData.postList?.length === 0) {
-                    setNextCursor(-1);
-                } else {
-                    setNextCursor(postData.postList[postData.postList.length - 1].postId);
-                }
-                console.log('postData', postData);
+                console.log('postData', postData.postList, postData.postList[postData.postList.length - 1].postId);
 
-                setPostList(newPostList);
+                console.log('set 후 nextCursor', nextCursor); // 0
+                console.log('postData', postData);
+                setIsReached(false);
             } catch (err) {
                 alert(err.message);
                 console.log(err.message);
             } finally {
                 setIsLoading(false);
+                console.log('finally nextCursor:', nextCursor); // 0
             }
         },
-        [nextCursor],
+        [isReached],
     );
 
-    const handleScroll = useCallback(
-        nextCursor => {
-            const scrollHeight = document.documentElement.scrollHeight;
-            const scrollTop = document.documentElement.scrollTop;
-            const clientHeight = document.documentElement.clientHeight;
-            console.log('height:', scrollHeight, 'top:', scrollTop, 'clientHeight:', clientHeight);
-            console.log('top+height=', scrollTop + clientHeight);
+    console.log('fetch 후 nextCursor', nextCursor); // 61
 
-            if (scrollTop + clientHeight >= scrollHeight && !isLoading && nextCursor !== -1) {
-                console.log(nextCursor);
-                // fetchPost(nextCursor);
-            }
-        },
-        [isLoading, fetchPost, nextCursor],
-    );
+    const handleScroll = useCallback(() => {
+        const scrollHeight = document.documentElement.scrollHeight;
+        const scrollTop = document.documentElement.scrollTop;
+        const clientHeight = document.documentElement.clientHeight;
+        // console.log('height:', scrollHeight, 'top:', scrollTop, 'clientHeight:', clientHeight);
+        // console.log('top+height=', scrollTop + clientHeight);
 
+        if (scrollTop + clientHeight >= scrollHeight) {
+            setIsReached(true);
+            console.log('isreached', isReached);
+        }
+    }, []);
+
+    console.log('isreached', isReached);
     useEffect(() => {
         // 페이지 초기 렌더링 시에 postList를 불러오기 위해 fetchPost 호출
         fetchPost(nextCursor);
         // 스크롤 이벤트 핸들러 등록 및 해제
         window.addEventListener('scroll', handleScroll);
-        console.log('nextCursor', nextCursor);
+        // console.log('nextCursor', nextCursor);
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
-    }, [nextCursor]);
+    }, [fetchPost]);
 
     // useEffect(() => {
     //     fetchPost(0);
@@ -100,6 +106,7 @@ function Story() {
                     </div>
                 ))}
                 {isLoading && <p>Loading...</p>}
+                {nextCursor === -1 && <p>데이터 로딩 완료!</p>}
             </div>
         </>
     );
