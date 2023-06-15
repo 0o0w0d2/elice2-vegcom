c97b9b0972ab6379509a748f8e4715e7327c4f3d
Jay Jung <wjdwogns120523@gmail.com>
2023-06-16 00:37:50 +0900

    충돌수정

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,7 @@
 // front/postcss.config.js
 module.exports = {
     plugins: {
-<<<<<<< HEAD
         tailwindcss: {},
         autoprefixer: {},
     },
 };
-=======
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  }
->>>>>>> f74ae47 (Style:일반커밋)
