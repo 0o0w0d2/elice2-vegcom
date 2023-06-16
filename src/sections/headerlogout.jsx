b170f825e9844101524b017c7130d151a0cbe812
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-06-16 16:27:25 +0900

    fix. headerlogout 안 쓰는 함수 삭제

--- src/sections/headerlogout.jsx
+++ src/sections/headerlogout.jsx
@@ -1,49 +1,6 @@
-import React, { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { DispatchContext } from '../../App';
-
-import { Fragment } from 'react';
-import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react';
-import { ChevronDownIcon } from '@heroicons/react/20/solid';
-import {
-    UserCircleIcon,
-    ChatBubbleLeftRightIcon,
-    UserIcon,
-    TrophyIcon,
-    ArrowLeftOnRectangleIcon,
-    ArrowPathIcon,
-    Bars3Icon,
-    ChartPieIcon,
-    CursorArrowRaysIcon,
-    FingerPrintIcon,
-    SquaresPlusIcon,
-    XMarkIcon,
-} from '@heroicons/react/24/outline';
+import React from 'react';
 
 function HeaderLogout() {
-    const userId = localStorage.getItem('userId');
-    const mymenu = [
-        { name: '마이페이지', description: '마이페이지', href: `/mypage/${userId}`, icon: UserCircleIcon },
-        { name: '정보 수정', description: '유저 정보 수정', href: `/useredit`, icon: UserIcon },
-        // {name: '쇼핑'},
-    ];
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-    const dispatch = useContext(DispatchContext);
-    const navigate = useNavigate();
-
-    const logout = () => {
-        localStorage.removeItem('userToken');
-        dispatch({ type: 'LOGOUT' });
-        navigate('/');
-    };
-
-    function classNames(...classes) {
-        return classes.filter(Boolean).join(' ');
-    }
-
-    const [isLogoHovered, setIsLogoHovered] = useState(false);
-
     return (
         <header className="fixed top-0 left-0 right-0 z-50" style={{ backgroundColor: 'white' }}>
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
