cd909c7b769bccbf22840f42ea0ad0188a2b9bdb
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-06-16 22:49:10 +0900

    fix. usercard, rankcard 디자인 수정

--- src/components/rankcard/rankcard.jsx
+++ src/components/rankcard/rankcard.jsx
@@ -13,25 +13,28 @@ const RankCard = ({ user, index }) => {
         }
     };
     return (
-        <div className="flex p-4 m-2 bg-white shadow-lg rounded-xl" style={{ width: '60vh', height: '20vh' }}>
+        <div className="flex p-4 m-2 bg-white shadow-lg rounded-xl" style={{ width: '65vh', height: '25vh' }}>
             <div className="flex flex-row justify-between items-center text-center">
-                <p style={{ color: '#008762' }} className="ml-5 mr-5 text-xl font-bold">
+                <p style={{ color: '#008762', fontSize: '3rem' }} className="ml-5 mr-5 text-xl font-bold">
                     {index}
                 </p>
-                <img className="mr-5 w-20 h-20 object-cover rounded-full" src={getImageSrc()} alt={user.id} />
-                <div className="w-full">
+                <img style={{ height: '15vh' }} className="mr-5 object-cover rounded-full" src={getImageSrc()} alt={user.id} />
+                <div className="ml-3 w-full">
                     <div className="text-left ">
-                        <p style={{ fontFamily: 'SUITE-Regular' }} className="text-lg font-bold">
+                        <p style={{ fontFamily: 'SUITE-Regular', fontSize: '1.5rem' }} className="text-lg mb-2">
                             {user.nickname}
                         </p>
-                        <p className="text-sm">누적 포인트: {user.accuPoint}</p>
-                        <p className="text-sm">총 게시물 수: {user.storyCount}</p>
-                        <p className="text-sm flex items-center">
+                        <p className="text-md">누적 포인트: {user.accuPoint}</p>
+                        <p className="text-md">총 게시물 수: {user.storyCount}</p>
+                        <p className="text-md flex items-center">
                             등급 :
-                            <img className='w-5 h-5 ml-2' src={TierDecision(user.accuPoint)[2]} alt={TierDecision(user.accuPoint)[0]} />
+                            <img
+                                className="w-5 h-5 ml-2"
+                                src={TierDecision(user.accuPoint)[2]}
+                                alt={TierDecision(user.accuPoint)[0]}
+                            />
                             {TierDecision(user.accuPoint)[1]}
                         </p>
-
                     </div>
                 </div>
             </div>
--- src/components/user/usercard.jsx
+++ src/components/user/usercard.jsx
@@ -12,28 +12,33 @@ const UserCard = ({ user, point }) => {
         }
     }, [user]);
 
-    console.log(TierDecision(point)[2])
     return (
         <>
             <div
                 className="flex items-center justify-center p-4 m-2 bg-white shadow-lg rounded-xl"
-                style={{ width: '60vh', height: '20vh' }}>
-                 <div className="flex flex-row justify-center items-center text-center">
-                    <img className="w-20 h-20object-cover rounded-full mb-2 mr-5" src={getImageSrc()} alt={user.id} />
+                style={{ width: '65vh', height: '25vh' }}>
+                <div className="flex w-full p-2 flex-row justify-center items-center text-center">
+                    <img
+                        style={{ height: '15vh' }}
+                        className="object-cover rounded-full mb-2 mr-5"
+                        src={getImageSrc()}
+                        alt={user.id}
+                    />
                     <div>
-                        <div>
+                        <div style={{ fontSize: '1.5rem' }}>
                             <span style={{ fontWeight: 'bold' }}> {user.nickname}</span> 님은 지금까지{' '}
-                            <span style={{ color: '#008762', fontWeight: 'bold' }}>{GetDays(user.createAt)}</span>일 동안 총{' '}
-                            <span style={{ fontWeight: 'bold', color: '#008762' }}>{user.storyCount} </span>끼 채식을 했어요!
+                            <span style={{ color: '#008762', fontWeight: 'bold' }}>{GetDays(user.createAt)}</span>일 동안{' '}
+                            <br></br>총<span style={{ fontWeight: 'bold', color: '#008762' }}>{user.storyCount} </span>끼 채식을
+                            했어요!
                         </div>
-                        <p className="text-sm">현재 누적 포인트: {point}</p>
-                        <p className="text-sm flex items-center">
-                            오늘 순위: {user.TodayRanking}위 전체 누적 순위: {user.AccuRanking}위 등급 :
-                            <img className='w-5 h-5 ml-2' src={TierDecision(point)[2]} alt={TierDecision(point)[0]} />
-                            {TierDecision(point)[1]}
-                        </p>
-
-                        
+                        <div>
+                            <p className="text-md">현재 누적 포인트: {point}</p>
+                            <p className="text-md flex items-center">
+                                오늘 순위: {user.TodayRanking}위 전체 누적 순위: {user.AccuRanking}위 등급 :
+                                <img className="w-5 h-5 ml-2" src={TierDecision(point)[2]} alt={TierDecision(point)[0]} />
+                                {TierDecision(point)[1]}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
