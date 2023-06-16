7b7e1e282d5f59698647487c3261564315757c52
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-06-16 23:46:10 +0900

    fix.useredit 페이지 디자인 수정

--- src/components/user/useredit.jsx
+++ src/components/user/useredit.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, useState } from 'react';
+import React, { useEffect, useCallback, useState, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserStateContext, DispatchContext } from '../../../App';
 import { UserCircleIcon } from '@heroicons/react/24/solid';
@@ -10,6 +10,11 @@ function UserEdit() {
     const [nickname, setNickname] = useState('');
     const [description, setDescription] = useState('');
     const [userImage, setUserImage] = useState('');
+
+    const isDesValid = useMemo(() => description.length <= 200, [description]);
+    const isNicknameValid = useMemo(() => nickname.length < 10 && nickname.length >= 2);
+
+    const isFormValid = useMemo(() => isDesValid && isNicknameValid, [isDesValid, isNicknameValid]);
 
     const handleSubmit = async userId => {
         try {
@@ -33,9 +38,12 @@ function UserEdit() {
                 setDescription(userData.description);
                 setNickname(userData.nickname);
                 setUserImage(userData.userImage);
-                console.log('user', userData);
             } catch (err) {
-                alert(err.message);
+                if (err.response.data.message) {
+                    alert(err.response.data.message);
+                } else {
+                    alert('라우팅 경로가 잘못되었습니다.');
+                }
             }
         },
         [userId],
@@ -45,11 +53,14 @@ function UserEdit() {
         fetchUser(userId);
     }, []);
     return (
-        <div className="userEditForm relative flex justify-center">
+        <div className="userEditForm relative flex justify-center" style={{ marginTop: '-10rem' }}>
             <div>
-                <h2 className="font-semibold">유저 정보 변경</h2>
+                <h2 style={{ color: '#008762', fontWeight: 'bold', fontSize: '2rem', marginBottom: '1rem' }}>유저 정보 변경</h2>
                 <div className="formSection flex flex-col mt-10" style={{ width: '40vw', minWidth: '350px' }}>
-                    <label htmlFor="username" className="text-left block text-sm font-medium leading-6 text-gray-900">
+                    <label
+                        style={{ fontWeight: 'bold' }}
+                        htmlFor="username"
+                        className="text-left  block text-md font-medium leading-6 text-gray-900">
                         닉네임
                     </label>
                     <div className="userNickname w-full mt-2">
@@ -61,10 +72,22 @@ function UserEdit() {
                                 value={nickname}
                                 onChange={e => setNickname(e.target.value)}></textarea>
                         </div>
-                        {/* 글자수 제한 코드 추가하기 */}
+                        <div>
+                            {!isNicknameValid && nickname.length === 0 && <p className="text-sm mt-2">닉네임을 입력해 주세요.</p>}
+                            {!isNicknameValid && nickname.length < 2 && (
+                                <p className="text-sm mt-2">닉네임은 2글자 이상으로 설정해 주세요.</p>
+                            )}
+
+                            {!isNicknameValid && nickname.length > 10 && (
+                                <p className="text-sm mt-2">닉네임은 10글자 이하로 설정해 주세요.</p>
+                            )}
+                        </div>
                     </div>
 
-                    <label htmlFor="userImage" className="text-left mt-7 block text-sm font-medium leading-6 text-gray-900">
+                    <label
+                        htmlFor="userImage"
+                        style={{ fontWeight: 'bold' }}
+                        className="text-left  mt-7 block text-md font-medium leading-6 text-gray-900">
                         프로필 사진
                     </label>
                     <div className="imageSection grid-cols-2">
@@ -77,7 +100,10 @@ function UserEdit() {
                                 multiple></input>
                         </div>
                     </div>
-                    <label htmlFor="about" className="text-left mt-7 block text-sm font-medium text-gray-900">
+                    <label
+                        htmlFor="about"
+                        style={{ fontWeight: 'bold' }}
+                        className=" text-left mt-7 block text-md font-medium text-gray-900">
                         자기소개
                     </label>
                     <div className="w-full mt-2">
@@ -86,18 +112,21 @@ function UserEdit() {
                             value={description}
                             onChange={e => setDescription(e.target.value)}
                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm "></textarea>
+                        <div>{!isDesValid && <p className="text-sm mt-2">자기소개는 200자까지 입력할 수 있습니다.</p>}</div>
                     </div>
-                    <div className="buttonSection mt-5 justify-center">
+                    <div style={{ marginTop: '5rem' }} className="buttonSection justify-center">
                         <button
                             onClick={() => navigate(-1)}
                             type="button"
-                            className=" shadow-sm text-sm font-semibold leading-6 text-gray-900">
+                            className="rounded-2xl px-10 py-2 text-black shadow-xl backdrop-blur-md transition-colors duration-30 mr-3">
                             취소
                         </button>
                         <button
                             onClick={() => handleSubmit(userId)}
                             type="submit"
-                            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            className={`rounded-2xl px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 ${
+                                isFormValid ? 'bg-[#008762] hover:bg-[#00B282]' : 'bg-gray-400 cursor-not-allowed'
+                            }`}>
                             수정하기
                         </button>
                     </div>
